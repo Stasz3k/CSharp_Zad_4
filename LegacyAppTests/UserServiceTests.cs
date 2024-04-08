4c22600948b27c7701bfd670836ce828971f7e22
Stasz3k <s24412@pjwstk.edu.pl>
2024-04-08 23:52:07 +0200

    SetUp w klasie testowej

--- LegacyAppTests/UserServiceTests.cs
+++ LegacyAppTests/UserServiceTests.cs
@@ -3,13 +3,19 @@ namespace LegacyApp.Tests;
 [TestFixture]
 public class UserServiceTests
 {
-
+    private UserService _userService;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _userService = new UserService();
+    }
+    
+    
     [Test]
     public void AddUser_Invalid_FirstName_ReturnsFalse()
     {
-        var userService = new UserService();
-        
-        var firstName = userService.AddUser("", "Doe", "test@example.com", new DateTime(1990, 1, 1), 1);
+        var firstName = _userService.AddUser("", "Doe", "test@example.com", new DateTime(1990, 1, 1), 1);
         
         Assert.IsFalse(firstName);
     }
@@ -17,8 +23,7 @@ public class UserServiceTests
     [Test]
     public void AddUser_Invalid_LastName_ReturnsFalse()
     {
-        var userService = new UserService();
-        var lastName = userService.AddUser("John", "", "test@example.com", new DateTime(1980, 1, 1), 1);
+        var lastName = _userService.AddUser("John", "", "test@example.com", new DateTime(1980, 1, 1), 1);
         
         Assert.IsFalse(lastName);
     }
@@ -26,9 +31,7 @@ public class UserServiceTests
     [Test]
     public void AddUser_Invalid_Email_ReturnsFalse()
     {
-        var userService = new UserService();
-
-        var result = userService.AddUser("John", "Doe", "invalid_email", new DateTime(1990, 1, 1), 1);
+        var result = _userService.AddUser("John", "Doe", "invalid_email", new DateTime(1990, 1, 1), 1);
         
         Assert.IsFalse(result);
     }
@@ -36,9 +39,7 @@ public class UserServiceTests
     [Test]
     public void AddUser_TooYoung_ReturnsFalse()
     {
-        var userService = new UserService();
-        
-        var result = userService.AddUser("John", "Doe", "test@example.com", DateTime.Now.AddYears(-20), 1);
+        var result = _userService.AddUser("John", "Doe", "test@example.com", DateTime.Now.AddYears(-20), 1);
         
         Assert.IsFalse(result);
     }
@@ -48,9 +49,7 @@ public class UserServiceTests
     [Test]
     public void IsValid_Input_ReturnsTrue()
     {
-        var userService = new UserService();
-
-        var isValidInput = userService.AddUser("John", "Doe", "test@example.com", new DateTime(1990, 1, 1), 1);
+        var isValidInput = _userService.AddUser("John", "Doe", "test@example.com", new DateTime(1990, 1, 1), 1);
            
         Assert.IsTrue(isValidInput);
     }
